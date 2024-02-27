db26a8c8cc3098c5bf8882c00762b2824602acf2
MathieuVDB28 <mathios2000@gmail.com>
2024-02-27 20:01:02 +0100

    add react_app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql_db',
+      port: 3306,
+      username: 'hot_combien_user',
+      password: 'passworduser',
+      database: 'hot_combien_app',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
